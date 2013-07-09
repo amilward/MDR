308f8e79e08448658b936dddfbe3029563ebf0b4
amilward <adam.milward@outlook.com>
2013-07-09 18:19:16 +0100

    adding conceptual domain value domain

---
+++ groovy_metadata_registry/grails-app/domain/uk/co/mdc/model/DataElementValueDomain.groovy
@@ -0,0 +1,36 @@
+package uk.co.mdc.model
+
+class DataElementValueDomain {
+	
+	DataElement dataElement
+	
+	ValueDomain valueDomain
+
+    static constraints = {
+    }
+	
+	static DataElementValueDomain link(DataElement, ValueDomain) {
+		def m = DataElementValueDomain.findByDataElementAndValueDomain(DataElement, ValueDomain)
+		if (!m)
+		{
+			m = new DataElementValueDomain()
+			DataElement?.addToDataElementValueDomains(m)
+			ValueDomain?.addToDataElementValueDomains(m)
+			m.save()
+		}
+		return m
+	}
+
+	static void unlink(DataElement, ValueDomain) {
+		def m = DataElementValueDomain.findByDataElementAndValueDomain(DataElement, ValueDomain)
+		if (m)
+		{
+			DataElement?.removeFromDataElementValueDomains(m)
+			ValueDomain?.removeFromDataElementValueDomains(m)
+			m.delete()
+		}
+	}
+	
+	
+	
+}
---
+++ groovy_metadata_registry/grails-app/domain/uk/co/mdc/model/ValueDomainConceptualDomain.groovy
@@ -0,0 +1,11 @@
+package uk.co.mdc.model
+
+class ValueDomainConceptualDomain {
+
+	ValueDomain valueDomain
+	
+	ConceptualDomain conceptualDomain
+	
+    static constraints = {
+    }
+}
