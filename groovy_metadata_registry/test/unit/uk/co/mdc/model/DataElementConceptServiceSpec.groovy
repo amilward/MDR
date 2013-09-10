d84d6ee1bc1d3833335bd4eda9eac832456fbe06
Adam Milward <adam.milward@outlook.com>
2013-09-10 16:27:13 +0100

    committing changes RE: refactoring the data element concept controller into a service for additional security

---
+++ groovy_metadata_registry/test/unit/uk/co/mdc/model/DataElementConceptServiceSpec.groovy
@@ -0,0 +1,17 @@
+package uk.co.mdc.model
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(DataElementConceptService)
+class DataElementConceptServiceSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
