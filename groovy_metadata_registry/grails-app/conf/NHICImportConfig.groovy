d0f64f75891d46df98f85e0d6aee60e02e69a0c0
James <james.welch@cs.ox.ac.uk>
2013-11-03 12:45:43 +0000

    - added NHIC data csvs - import the ACS dataset

--- groovy_metadata_registry/grails-app/conf/NHICImportConfig.groovy
+++ groovy_metadata_registry/grails-app/conf/NHICImportConfig.groovy
@@ -6,7 +6,8 @@ import uk.co.mdc.model.ValueDomain
 
 class NHICImportConfig {
 
-	public static functions = [ '/WEB-INF/bootstrap-data/NHIC/CAN/CAN.csv' : 
+	public static functions = [ 
+		'/WEB-INF/bootstrap-data/NHIC/Initial/CAN.csv' : 
 		{ tokens -> 
 			def categories = [tokens[2], tokens[1], "Initial Proposal - CUH","Ovarian Cancer", "NHIC Datasets"];
 			def dec = importDataElementConcepts(categories, null);
@@ -23,9 +24,29 @@ class NHICImportConfig {
 										refId: tokens[0]).save(failOnError: true)
 			de.dataElementValueDomains().add(vd);
 			de.save();
-		}
+			println "importing: " + tokens[0]
+		},
+	'/WEB-INF/bootstrap-data/NHIC/Initial/ACS.csv' :
+	{ tokens ->
+		def categories = [tokens[2], tokens[1], "Initial Proposal - IMP","Acute Coronary Syndromes", "NHIC Datasets"];
+		def dec = importDataElementConcepts(categories, null);
+		def ext = new JSONObject();
+		ext.put("NHIC Identifier", tokens[0]);
+		ext.put("Local Identifier", tokens[7]);
+		ext.put("Data Dictionary Element",tokens[6]);
+		def vd = new ValueDomain('name' : "Value domain for " + tokens[0], 'refId' : tokens[0],'description' : tokens[5]).save(failOnError: true);
+		def de = new DataElement(	name: tokens[3],
+									description : tokens[4],
+									dataElementConcept: dec,
+									extension: ext,
+									refId: tokens[0]).save(failOnError: true)
+		de.dataElementValueDomains().add(vd);
+		de.save();
+		println "importing: " + tokens[0]
+	}
 		
 	]
+		
 	
 	static private importDataElementConcepts(nodenames, parent)
 	{
