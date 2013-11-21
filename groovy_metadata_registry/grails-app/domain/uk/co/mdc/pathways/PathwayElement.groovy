2c3a68a998a5b6aff394d026fe51ba516053f4dd
Ryan Brooks <ryanbrooksis@gmail.com>
2013-11-21 15:11:56 +0000

    Removing getJSON methods

--- groovy_metadata_registry/grails-app/domain/uk/co/mdc/pathways/PathwayElement.groovy
+++ groovy_metadata_registry/grails-app/domain/uk/co/mdc/pathways/PathwayElement.groovy
@@ -14,51 +14,7 @@ abstract class PathwayElement extends ExtensibleObject  {
 	String description
 
 	Collection peCollection
-	
-	String GetElementsJSON(){
-		def result = []
-		def de = new StringBuffer()
-		def isFirst = new Boolean('True')
-
-		Iterator i = peCollection.dataElementCollections.iterator();		
-		while (i.hasNext()){
-			DataElementCollection it = i.next()
-			if(isFirst){
-
-				de.append('{"id" : "' + it.dataElement.refId + '", "description" : "' + it.dataElement.description + '"}')
-			}else{
-				de.append(',')
-				de.append('{"id" : "' + it.dataElement.refId + '", "description" : "' + it.dataElement.description + '"}')
-			}
-			isFirst = new Boolean('False')
-		}
 		
-		result = de.toString()
-	}
-	
-	String GetFormsJSON(){
-		def result = []
-		def de = new StringBuffer()
-		def isFirst = new Boolean('True')
-
-		Iterator i = peCollection.forms.iterator();
-		while (i.hasNext()){
-			FormDesign it = i.next()
-			if(isFirst){
-
-				de.append('{"id" : "' + it.name + '", "description" : "' + it.description + '","version" : "' + it.versionNo + '"}')
-			}else{ 
-				de.append(',')
-				de.append('{"id" : "' + it.name + '", "description" : "' + it.description + '","version" : "' + it.versionNo + '"}')
-			}
-			isFirst = new Boolean('False')
-		}
-		
-		result = de.toString()
-	}
-
-
-	
 	static hasMany = [mandatoryInputs: Collection,
 					  mandatoryOutputs: Collection,
 					  optionalInputs: Collection,
