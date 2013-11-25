bcbe0f41cfac8d0132e28320e9b44f967caa4697
Ryan Brooks <ryanbrooksis@gmail.com>
2013-11-25 17:26:40 +0000

    Updating gitignoer git add .gitignore:

--- groovy_metadata_registry/grails-app/conf/BuildConfig.groovy
+++ groovy_metadata_registry/grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
         // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
