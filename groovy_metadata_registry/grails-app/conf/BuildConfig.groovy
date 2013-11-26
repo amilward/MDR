44b6714c2f1c983ce22501544868d223cdd150ee
Ryan Brooks <ryanbrooksis@gmail.com>
2013-11-26 10:16:27 +0000

    Updated build config with merge from MC-58

--- groovy_metadata_registry/grails-app/conf/BuildConfig.groovy
+++ groovy_metadata_registry/grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,4 @@
-	grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
         // specify dependency exclusions here; for example, uncomment this to disable ehcache:
         // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
