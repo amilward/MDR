b6075d2cb5709aff67f3d31a35c3c76216bb53f6
Charles Crichton <charles.crichton@ndm.ox.ac.uk>
2013-11-29 17:15:31 +0000

    End of play on Friday

--- groovy_metadata_registry/grails-app/domain/uk/co/mdc/pathways/PathwaysModel.groovy
+++ groovy_metadata_registry/grails-app/domain/uk/co/mdc/pathways/PathwaysModel.groovy
@@ -53,9 +53,46 @@ class PathwaysModel  {
 		return links
 		
 	}
-	
+		
+	/**
+	 * Static method parsing an input stream containing XML definitions conforming to the
+	 * <code>http://pathways.mdc.co.uk/1/pm.xsd</code> schema.
+	 * @author Charles Crichton
+	 * @param inputStream The input stream to be parsed. 
+	 * @return Collection of loaded PathwayModels
+	 * @throws 
+	 */
 	static List<PathwaysModel> loadXML(InputStream inputStream) {
-		return [];
+		
+		final def ns_pm = "http://pathways.mdc.co.uk/1/pm.xsd"
+		
+		println(inputStream.getClass().toString())
+		
+		groovy.util.slurpersupport.NodeChild slurper = new XmlSlurper().parse(inputStream).declareNamespace(pm: ns_pm)
+		
+		def pathwaysModels = [];
+		
+		
+		
+		//Check that there is at least a root element with the namespace
+		{
+			def namespaceURI = slurper.namespaceURI();
+		}
+		if (namespaceURI.equals(ns_pm)) {
+			throw new RuntimeException("Unknown namespace in supplied XML: "+namespaceURI)
+		}
+		}
+		
+		slurper."pm:PathwaysModels".each {		
+
+			def pathwaysModel = new PathwaysModel()
+			pathwaysModel.slurp(item)
+						
+			//We get this far if there are no exceptions, so add to the list
+			pathwaysModels += pathwaysModel
+		}
+		
+		return pathwaysModels;
 	}
 	
 }
--- groovy_metadata_registry/test/unit/uk/co/mdc/pathways/PathwaysModelSlurperSpec.groovy
+++ groovy_metadata_registry/test/unit/uk/co/mdc/pathways/PathwaysModelSlurperSpec.groovy
@@ -1,7 +1,14 @@
+
+
+
 package uk.co.mdc.pathways
 
 import grails.test.mixin.*
+
 import org.junit.*
+
+import com.sun.xml.internal.ws.client.sei.ResponseBuilder.InputStreamBuilder;
+
 import groovy.xml.XmlUtil;
 
 /**
@@ -10,39 +17,59 @@ import groovy.xml.XmlUtil;
  */
 @TestFor(PathwaysModel)
 class PathwaysModelSlurperSpec extends spock.lang.Specification {
-	
+
+	//Section: Test basic loading and saving
+		
 	def "load pathway model that is empty" () {
-		when: "presented with an imput stream containing valid xml"
-		
-			def pathwaysModels = PathwaysModel.loadXML(
-				XmlUtil.asStreamSource(xml_pathways_models_empty).inputStream
-			);	
-		
-		then: "an empty collection of pathway models should be returned"
-		
+		when: "presented with an imput stream containing valid xml"		
+			def pathwaysModels = loadPathwaysModels(xml_pathways_models_empty)
+					
+		then: "an empty collection of pathway models should be returned"		
 			pathwaysModels == [];		
 	} 
 	
-	def "throw and exception when xml has no namespace" () {
-		when: "presented with an imput stream containing invalid xml"
+	def "throw an exception when xml has no namespace" () {
+		when: "presented with an imput stream containing invalid xml (no namespace)"
+			def pathwaysModels = loadPathwaysModels(xml_invalid_no_namespace)
 		
-			def pathwaysModels = PathwaysModel.loadXML(
-				XmlUtil.asStreamSource(xml_invalid_no_namespace).inputStream
-			);
-		
-		then: "an empty collection of pathway models should be returned"		
+		then: "an exception should be thrown"		
 			Throwable e = thrown()	
 	}
 	
-	/* Example pathways models for using in tests */
-	def xml_invalid_no_namespace = """
+	
+		
+	
+	/* XML Constants */
+	static final def xml_pi = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+	static final def ns_pm = "http://pathways.mdc.co.uk/1/pm.xsd"
+	static final def quoted_pmxsdns = "\""+ns_pm+"\""
+	
+	/* Turn Strings into streams */
+	
+	def List<PathwaysModel> loadPathwaysModels (String text) {
+		return PathwaysModel.loadXML(
+				convertToInputStream(xml_pathways_models_empty)
+		);	
+	}
+	
+	def InputStream convertToInputStream(String text) {
+		return new ByteArrayInputStream(text.getBytes(java.nio.charset.Charset.forName("UTF-8")))		
+	}
+	
+	/* Example pathways models for using in tests */	
+	static final def xml_invalid_corrupt = xml_pi+"""
+		<This is not valid XML>
+		<& neither is this!>
+	"""
+	
+	static final def xml_invalid_no_namespace = xml_pi+"""
 		<PathwaysModels>
 			
 		</PathwaysModels>
 	"""
 	
-	def xml_pathways_models_empty = """
-		<PathwaysModels xmlns="http://pathways.mdc.co.uk/1">
+	static final String xml_pathways_models_empty = xml_pi+"""
+		<PathwaysModels xmlns="""+quoted_pmxsdns+""">
 			
 		</PathwaysModels>
 """
