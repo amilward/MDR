72c921eb8faa3a8c69ff2d9b2c6729b3c1f9b40c
Ryan Brooks <ryanbrooksis@gmail.com>
2013-12-06 14:11:26 +0000

    Bootstrapping for production - single secure admin user to start with + option to import NHIC data

---
+++ groovy_metadata_registry/test/unit/uk/co/mdc/utils/importers/ImportNHICServiceSpec.groovy
@@ -0,0 +1,17 @@
+package uk.co.mdc.utils.importers
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ImportNHICService)
+class ImportNHICServiceSpec {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
