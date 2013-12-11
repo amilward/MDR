1028204e315eeb7715741a42d9b5264f44fcd9c0
Charles Crichton <charles.crichton@ndm.ox.ac.uk>
2013-12-11 18:15:40 +0000

    Added tests and code.

--- groovy_metadata_registry/test/unit/uk/co/mdc/loaders/PathwaysModelLoaderSpec.groovy
+++ groovy_metadata_registry/test/unit/uk/co/mdc/loaders/PathwaysModelLoaderSpec.groovy
@@ -1,3 +1,4 @@
+
 package uk.co.mdc.loaders
 
 import grails.test.mixin.*
@@ -7,13 +8,14 @@ import org.junit.*
 import com.sun.xml.internal.ws.client.sei.ResponseBuilder.InputStreamBuilder;
 
 import groovy.xml.XmlUtil;
+import uk.co.mdc.pathways.*;
 
 /**
  * Unit tests for XML Slurping of Pathways Models
  * @author Charles Crichton (Charles.Crichton@ndm.ox.ac.uk)
  */
-@TestFor(PathwaysModelLoader)
-class PathwaysModelSlurperSpec extends spock.lang.Specification {
+//@TestFor(PathwaysModelLoader)
+class PathwaysModelLoaderSpec extends spock.lang.Specification {
 
 	/* XML Constants */
 	
@@ -39,16 +41,16 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 	"""
 	
 	def "throw an exception when xml is corrupt" () {
-		when: "presented with an imput stream containing invalid xml (corrupt)"
-			def pathwaysModels = loadPathwaysModels(XML_INVALID_CORRUPT)
-		
+		when: "presented with an imput stream containing invalid xml (corrupt)"			
+			PathwaysModelsAndMapping pmam = loadPathwaysModels (XML_INVALID_CORRUPT)
+			
 		then: "an exception should be thrown"
 			RuntimeException e = thrown()
 	}
 	
 	def "throw an exception when xml has no namespace" () {
 		when: "presented with an imput stream containing invalid xml (no namespace)"
-			def pathwaysModels = loadPathwaysModels(XML_INVALID_NO_NAMESPACE)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_INVALID_NO_NAMESPACE)
 		
 		then: "an exception should be thrown"		
 			RuntimeException e = thrown()	
@@ -56,7 +58,7 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 	
 	def "throw an exception when root element is not a PathwaysModels element" () {
 		when: "presented with an imput stream containing invalid xml (incorrect root element name)"
-			def pathwaysModels = loadPathwaysModels(XML_INVALID_INCORRECT_ROOT_ELEMENT_NAME)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_INVALID_INCORRECT_ROOT_ELEMENT_NAME)
 		
 		then: "an exception should be thrown"
 			RuntimeException e = thrown()
@@ -72,10 +74,10 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 	
 	def "load pathway model that is empty" () {
 		when: "presented with an imput stream containing valid xml"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODELS_EMPTY)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODELS_EMPTY)
 					
 		then: "an empty collection of pathway models should be returned"
-			pathwaysModels == [];
+			pmam.pathwaysModels == [];
 	}
 	
 	/* Load single pathway */
@@ -122,68 +124,76 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 	"""
 	
 	def "PathwaysModel with no name attribute is invalid" () {
-		when: "PathwaysModel has no attributes"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_NO_ATTRIBUTES)
-		
+		when: "PathwaysModel has no attributes"			
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_NO_ATTRIBUTES)
+						
 		then: "an exception should be thrown"
 			RuntimeException e = thrown()				
 	}
 
 	def "PathwaysModel with just name attribute" () {
 		when: "PathwaysModel has no attributes"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_JUST_NAME_ATTRIBUTE)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_JUST_NAME_ATTRIBUTE)
 		
 		then: "the fields are instantiated"
-			assert pathwaysModels[0].name.equals("name")
-			assert pathwaysModels[0].versionNo == null
-			assert pathwaysModels[0].isDraft == false
-			assert pathwaysModels[0].description == null
-			assert pathwaysModels[0].getNodes().isEmpty()
-			assert pathwaysModels[0].getLinks().isEmpty()
+			PathwaysModel pathwaysModel = pmam.pathwaysModels[0]
+		
+			assert pathwaysModel.name.equals("name")
+			assert pathwaysModel.versionNo == null
+			assert pathwaysModel.isDraft == false
+			assert pathwaysModel.description == null
+			assert pathwaysModel.getNodes().isEmpty()
+			assert pathwaysModel.getLinks().isEmpty()
 	}
 	
 	def "PathwaysModel has some attributes 1" () {
 		when: "PathwaysModel has some attributes 1"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_ATTRIBUTES_1)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_ATTRIBUTES_1)
 		
 		then: "the fields are instantiated"
-			assert pathwaysModels[0].name.equals("name")
-			assert pathwaysModels[0].versionNo.equals("1.0")
-			assert pathwaysModels[0].isDraft == true
-			assert pathwaysModels[0].description == null
-			assert pathwaysModels[0].getNodes().isEmpty()
-			assert pathwaysModels[0].getLinks().isEmpty()
+			PathwaysModel pathwaysModel = pmam.pathwaysModels[0]
+		
+			assert pathwaysModel.name.equals("name")
+			assert pathwaysModel.versionNo.equals("1.0")
+			assert pathwaysModel.isDraft == true
+			assert pathwaysModel.description == null
+			assert pathwaysModel.getNodes().isEmpty()
+			assert pathwaysModel.getLinks().isEmpty()
 	}
 	
 	def "PathwaysModel has some attributes 2" () {
 		when: "PathwaysModel has some attributes 2"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_ATTRIBUTES_2)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_ATTRIBUTES_2)
 		
 		then: "the fields are instantiated"
-			assert pathwaysModels[0].name.equals("name")
-			assert pathwaysModels[0].versionNo.equals("1.0")
-			assert pathwaysModels[0].isDraft == false
-			assert pathwaysModels[0].description == null
-			assert pathwaysModels[0].getNodes().isEmpty()
-			assert pathwaysModels[0].getLinks().isEmpty()
+			PathwaysModel pathwaysModel = pmam.pathwaysModels[0]
+		
+			assert pathwaysModel.name.equals("name")
+			assert pathwaysModel.versionNo.equals("1.0")
+			assert pathwaysModel.isDraft == false
+			assert pathwaysModel.description == null
+			assert pathwaysModel.getNodes().isEmpty()
+			assert pathwaysModel.getLinks().isEmpty()
 	}
 	
 	def "PathwaysModel has a description 1" () {
 		when: "PathwaysModel has a description 1"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_DESCRIPTION_1)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_DESCRIPTION_1)
 		
 		then: "the fields are instantiated"
-			assert pathwaysModels[0].name.equals("name")
-			assert pathwaysModels[0].versionNo.equals("1.0")
-			assert pathwaysModels[0].isDraft == false
-			assert pathwaysModels[0].description.equals("desc")
-			assert pathwaysModels[0].getNodes().isEmpty()
-			assert pathwaysModels[0].getLinks().isEmpty()
+			PathwaysModel pathwaysModel = pmam.pathwaysModels[0]
+		
+			assert pathwaysModel.name.equals("name")
+			assert pathwaysModel.versionNo.equals("1.0")
+			assert pathwaysModel.isDraft == false
+			assert pathwaysModel.description.equals("desc")
+			assert pathwaysModel.getNodes().isEmpty()
+			assert pathwaysModel.getLinks().isEmpty()
 	}
 	
 	def "PathwaysModel has too many descriptions" () {
 		when: "PathwaysModel has too many descriptions"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_TOO_MANY_DESCRIPTIONS)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_TOO_MANY_DESCRIPTIONS)
 		
 		then: "an exception should be thrown"
 			RuntimeException e = thrown()	
@@ -212,27 +222,28 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 	
 	def "PathwaysModel has a node" () {
 		when: "PathwaysModel has a node"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_ONE_NODE)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_WITH_ONE_NODE)
 		
-		then: "the transientId is instantiated"
-			PathwaysModel pathwaysModel = pathwaysModels[0]
-			Node node1 = pathwaysModel.getNodes()[0]
-			assert node1.transientId == "id.1"
+		then: "the tId is instantiated"
+			PathwaysModel pathwaysModel = pmam.pathwaysModels[0]
+			HashMap<String,PathwayElement> idToPathwayElement = pmam.idToPathwayElementList[0]
+			Node node1 = pathwaysModel.getNodes()[0]			
+			assert idToPathwayElement["id.1"] == node1
 			assert node1.pathwaysModel == pathwaysModel
 			assert pathwaysModel.parentNode == null	
 	}
 	
 	def "PathwaysModel has two nodes and a link" () {
 		when: "PathwaysModel has two nodes and one link"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_TWO_NODES_ONE_LINK)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_TWO_NODES_ONE_LINK)
 		
 		then: "the link contains the nodes"
-			PathwaysModel pathwaysModel = pathwaysModels[0]
-			Node node1 = pathwaysModel.getNodes().find { it.transientId.equals("id.1") }
-			Node node2 = pathwaysModel.getNodes().find { it.transientId.equals("id.2") }
-			Link link1 = pathwaysModel.getLinks()[0];
-			assert node1.transientId == "id.1"
-			assert node2.transientId == "id.2"
+			PathwaysModel pathwaysModel = pmam.pathwaysModels[0]
+			HashMap<String,PathwayElement> idToPathwayElement = pmam.idToPathwayElementList[0]
+			
+			Node node1 = idToPathwayElement["id.1"]  
+			Node node2 = idToPathwayElement["id.2"]  
+			Link link1 = pathwaysModel.getLinks()[0];			
 			assert link1.source == node1
 			assert link1.target == node2
 			assert node1.pathwaysModel == pathwaysModel		
@@ -286,27 +297,25 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 	
 	def "PathwaysModel has a node with a subpathway that has two nodes and a link" () {
 		when: "PathwaysModel has two nodes and one link"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_SUBPATHWAY_TWO_NODES_ONE_LINK)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_SUBPATHWAY_TWO_NODES_ONE_LINK)
 		
 		then: "the pathway is set up as follows"
-			PathwaysModel pathwaysModel = pathwaysModels[0];
-			Node node1 = pathwaysModel.getNodes().find { it.transientId.equals("id.1") }
+			PathwaysModel pathwaysModel = pmam.pathwaysModels[0]
+			HashMap<String,PathwayElement> idToPathwayElement = pmam.idToPathwayElementList[0]
+			
+			Node node1 = idToPathwayElement["id.1"]
 			PathwaysModel subPathwaysModel = node1.subModel
 			assert subPathwaysModel != null
 			
-			Node node2 = subPathwaysModel.getNodes().find { it.transientId.equals("id.2") }
+			Node node2 = idToPathwayElement["id.2"]
 			assert node2 != null
 						
-			Node node3 = subPathwaysModel.getNodes().find { it.transientId.equals("id.3") }
+			Node node3 = idToPathwayElement["id.3"]
 			assert node3 != null
 			
-			Link link1 = subPathwaysModel.getLinks().find { it.transientId.equals("l.1") }
+			Link link1 = idToPathwayElement["l.1"]
 			assert link1 != null
 			
-			assert node1.transientId == "id.1"
-			assert node2.transientId == "id.2"
-			assert node3.transientId == "id.3"
-			assert link1.transientId == "l.1"
 			assert link1.source == node2
 			assert link1.target == node3
 			
@@ -320,7 +329,7 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 	
  	def "PathwaysModel has a node with two subpathways is not allowed" () {
 		when: "PathwaysModel has two nodes and one link"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_MULTIPLE_SUBPATHWAY)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_MULTIPLE_SUBPATHWAY)
 				
 		then: "an exception should be thrown"
 		RuntimeException e = thrown()			
@@ -328,49 +337,43 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 	
 	def "PathwaysModel has two nodes with a subpathway that has two nodes and 3 links" () {
 		when: "PathwaysModel has two nodes and one link"
-			def pathwaysModels = loadPathwaysModels(XML_PATHWAYS_MODEL_SUBPATHWAY_FOUR_NODES_THREE_LINKS)
+			PathwaysModelsAndMapping pmam = loadPathwaysModels(XML_PATHWAYS_MODEL_SUBPATHWAY_FOUR_NODES_THREE_LINKS)
 		
 		then: "the pathway is set up as follows"
-			PathwaysModel pathwaysModel = pathwaysModels[0]
-			Node node1 = pathwaysModel.getNodes().find { it.transientId.equals("id.1") }
+			PathwaysModel pathwaysModel = pmam.pathwaysModels[0]
+			HashMap<String,PathwayElement> idToPathwayElement = pmam.idToPathwayElementList[0]
+			
+			Node node1 = idToPathwayElement["id.1"]
 			assert node1 != null
 			
 			
 			PathwaysModel subPathwaysModel = node1.subModel
 			assert subPathwaysModel != null
 			
-			Node node2 = subPathwaysModel.getNodes().find { it.transientId.equals("id.2") }
+			Node node2 = idToPathwayElement["id.2"]
 			assert node2 != null
 						
-			Node node3 = subPathwaysModel.getNodes().find { it.transientId.equals("id.3") }
+			Node node3 = idToPathwayElement["id.3"] 
 			assert node3 != null
 			
-			Node node4 = pathwaysModel.getNodes().find { it.transientId.equals("id.4") }
+			Node node4 = idToPathwayElement["id.4"]
 			assert node1 != null
 			
-			Link link1 = subPathwaysModel.getLinks().find { it.transientId.equals("l.1") }
+			Link link1 = idToPathwayElement["l.1"]
 			assert link1 != null
 
-			Link link2 = subPathwaysModel.getLinks().find { it.transientId.equals("l.2") }
+			Link link2 = idToPathwayElement["l.2"]
 			assert link2 != null
 
-			Link link3 = pathwaysModel.getLinks().find { it.transientId.equals("l.3") }
+			Link link3 = idToPathwayElement["l.3"]
 			assert link3 != null
-			
-			assert node1.transientId == "id.1"
-			assert node2.transientId == "id.2"
-			assert node3.transientId == "id.3"
-			assert node4.transientId == "id.4"
-			
-			assert link1.transientId == "l.1"
+					
 			assert link1.source == node2
 			assert link1.target == node3
-			
-			assert link2.transientId == "l.2"
+
 			assert link2.source == node3
 			assert link2.target == node4
 			
-			assert link3.transientId == "l.3"
 			assert link3.source == node1
 			assert link3.target == node4
 			
@@ -392,11 +395,32 @@ class PathwaysModelSlurperSpec extends spock.lang.Specification {
 		return new ByteArrayInputStream(text.getBytes(java.nio.charset.Charset.forName("UTF-8")))		
 	}
 	
-	def List<PathwaysModel> loadPathwaysModels (String text) {
-		return PathwaysModel.loadXML(
+	/**
+	 * Returns a list containing two items. The first is a list of loaded pathways the second item is a corresponding 
+	 * list of mappings from is to PathwayElement.
+	 * To use: 	
+	 * <code>PathwaysModelsAndMapping pmam = loadPathwaysModels (...)</code>
+	 * @param text The XML to be parsed.
+	 * @return
+	 */
+	
+	
+	protected PathwaysModelsAndMapping loadPathwaysModels (String text) {
+		
+		return new PathwaysModelsAndMapping(PathwaysModelLoader.mappedLoadXML(
 				convertToInputStream(text)
-		);
+		));
 	}
 	
-	
+	//Needed to work around issues with spock and returning multiple values from methods.
+	class PathwaysModelsAndMapping {
+		
+		public PathwaysModelsAndMapping(List<Object> obj) {
+			this.pathwaysModels = obj[0]
+			this.idToPathwayElementList = obj[1]			
+		}
+		
+		List<PathwaysModel> pathwaysModels;
+		List<HashMap<String,PathwayElement>> idToPathwayElementList 		
+	}
 }
