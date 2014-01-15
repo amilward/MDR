677bc169da553edf9496be0e244cdcac0168dcf2
Ryan Brooks <ryanbrooksis@gmail.com>
2014-01-15 13:37:02 +0000

    Adding test for pathways list

---
+++ test/functional/uk/co/mdc/pathways/PathwayListSpec.groovy
@@ -0,0 +1,47 @@
+package uk.co.mdc.pathways
+
+import geb.spock.GebReportingSpec
+import org.openqa.selenium.Dimension
+import uk.co.mdc.pages.DashboardPage
+import uk.co.mdc.pages.authentication.LoginPage
+import uk.co.mdc.pages.pathways.PathwayListPage
+
+/**
+ * A specification for lists of pathways, including the ones
+ * on the dashboard and pathways list page.
+ *
+ * Created by Ryan, Soheil and Susana on 15/01/2014.
+ */
+class PathwayListSpec extends GebReportingSpec{
+
+    def "Check that subpathways are not displayed in the big lists"(){
+
+        given: "I'm on the dashboard"
+        driver.manage().window().setSize(new Dimension(1028, 768))
+        to LoginPage
+        loginRegularUser()
+        at DashboardPage
+
+
+        //when: "I go to the dashboard pathways list screen"
+
+        //then: "The pathways list is displayed and only the top level pathways are listed."
+
+
+
+        when: "I go to the pathways list page"
+        nav.goToPathwayListPage()
+        at PathwayListPage
+
+        then: "The pathways list is displayed"
+        dataTableRows.displayed
+
+        and: "the pathways list contains a known top-level pathway"
+        dataTableRows.find("a", text: "Transplanting and Monitoring Pathway").displayed
+
+        and: "the pathways list does not contain a known subpathway"
+        !dataTableRows.find("a", text: "Guarding Patient on recovery and transfer to nursing ward").displayed
+
+
+    }
+}
